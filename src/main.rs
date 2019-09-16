2a1c00e832f3c7f0fb9ad1c19ce64f274262289f
mosh <mosh.ohayo@gmail.com>
2019-09-17 01:42:55 +0900

    Remove the duplications of the implementations.

--- src/main.rs
+++ src/main.rs
@@ -1,153 +1,111 @@
 #![feature(test)]
 
-extern crate rand;
 extern crate test;
+extern crate rand;
 
+use test::Bencher;
+use std::ops::{Add, Mul, BitXor};
+use rand::{random, Rng, distributions::{Distribution, Standard}};
 
-#[cfg(test)]
-mod tests {
-    use test::Bencher;
-
-    use rand::prelude::*;
-
-    #[bench]
-    fn bench_u8_add(b: &mut Bencher) {
-        let mut a = [0u8; 1000];
-        for i in a.iter_mut() {
-            *i = random();
-        }
-        b.iter(|| {
-            a.iter().fold(0, |i, &j| i + j)
-        })
+fn bench_add<T>(b: &mut Bencher) where T: Copy + Default + Add<Output=T>, Standard: Distribution<T> {
+    let mut nums = [T::default(); 1000];
+    for i in nums.iter_mut() {
+        *i = random();
     }
+    b.iter(|| {
+        nums.iter().fold(T::default(), |i, &j| i + j)
+    });
+}
 
-    #[bench]
-    fn bench_u16_add(b: &mut Bencher) {
-        let mut a = [0u16; 1000];
-        for i in a.iter_mut() {
-            *i = random::<u8>().into();
-        }
-        b.iter(|| {
-            a.iter().fold(0, |i, &j| i + j)
-        })
-    }
+#[bench]
+fn bench_add_u8(b: &mut Bencher) {
+    bench_add::<u8>(b);
+}
 
-    #[bench]
-    fn bench_u32_add(b: &mut Bencher) {
-        let mut a = [0u32; 1000];
-        for i in a.iter_mut() {
-            *i = random::<u8>().into();
-        }
-        b.iter(|| {
-            a.iter().fold(0, |i, &j| i + j)
-        })
-    }
+#[bench]
+fn bench_add_u16(b: &mut Bencher) {
+    bench_add::<u16>(b);
+}
 
-    #[bench]
-    fn bench_u64_add(b: &mut Bencher) {
-        let mut a = [0u64; 1000];
-        for i in a.iter_mut() {
-            *i = random::<u8>().into();
-        }
-        b.iter(|| {
-            a.iter().fold(0, |i, &j| i + j)
-        })
-    }
+#[bench]
+fn bench_add_u32(b: &mut Bencher) {
+    bench_add::<u32>(b);
+}
 
-    #[bench]
-    fn bench_u128_add(b: &mut Bencher) {
-        let mut a = [0u128; 1000];
-        for i in a.iter_mut() {
-            *i = random::<u8>().into();
-        }
-        b.iter(|| {
-            a.iter().fold(0, |i, &j| i + j)
-        })
-    }
+#[bench]
+fn bench_add_u64(b: &mut Bencher) {
+    bench_add::<u64>(b);
+}
 
-    #[bench]
-    fn bench_u8(b: &mut Bencher) {
-        let mut s = [0u8; 100];
-        for i in s.iter_mut() {
-            *i = random();
-        }
-        b.iter(|| {
-            s.iter().fold(0, |i, &j| (i * j) ^ j)
-        })
-    }
+#[bench]
+fn bench_add_u128(b: &mut Bencher) {
+    bench_add::<u128>(b);
+}
 
-    #[bench]
-    fn bench_u16(b: &mut Bencher) {
-        let mut s = [0u16; 100];
-        for i in s.iter_mut() {
-            *i = random::<u8>().into();
-        }
-        b.iter(|| {
-            s.iter().fold(0, |i, &j| (i * j) ^ j)
-        })
+fn bench_mul_xor<T>(b: &mut Bencher)
+    where T: Copy + Default + Mul<Output=T> + BitXor<Output=T>, Standard: Distribution<T>
+{
+    let mut nums = [T::default(); 100];
+    for i in nums.iter_mut() {
+        *i = random();
     }
+    b.iter(|| {
+        nums.iter().fold(T::default(), |i, &j| (i * j) ^ j)
+    });
+}
 
-    #[bench]
-    fn bench_u32(b: &mut Bencher) {
-        let mut s = [0u32; 100];
-        for i in s.iter_mut() {
-            *i = random::<u8>().into();
-        }
-        b.iter(|| {
-            s.iter().fold(0, |i, &j| (i * j) ^ j)
-        })
-    }
+#[bench]
+fn bench_mul_xor_u8(b: &mut Bencher) {
+    bench_mul_xor::<u8>(b);
+}
 
-    #[bench]
-    fn bench_u64(b: &mut Bencher) {
-        let mut s = [0u64; 100];
-        for i in s.iter_mut() {
-            *i = random::<u8>().into();
-        }
-        b.iter(|| {
-            s.iter().fold(0, |i, &j| (i * j) ^ j)
-        })
-    }
+#[bench]
+fn bench_mul_xor_u16(b: &mut Bencher) {
+    bench_mul_xor::<u16>(b);
+}
 
-    #[bench]
-    fn bench_u128(b: &mut Bencher) {
-        let mut s = [0u128; 100];
-        for i in s.iter_mut() {
-            *i = random::<u8>().into();
-        }
-        b.iter(|| {
-            s.iter().fold(0, |i, &j| (i * j) ^ j)
-        })
-    }
+#[bench]
+fn bench_mul_xor_u32(b: &mut Bencher) {
+    bench_mul_xor::<u32>(b);
+}
 
-    #[bench]
-    fn bench_rem_if(b: &mut Bencher) {
-        let mut rng = rand::thread_rng();
-        let mut a = [0u8; 128];
-        for i in a.iter_mut() {
-            *i = rng.gen_range(0, 30);
-        }
-        b.iter(|| {
-            a.iter().fold(0, |i, &j| {
-                let i = i + j;
-                if i < 30 {
-                    i
-                } else {
-                    i - 30
-                }
-            })
-        })
-    }
+#[bench]
+fn bench_mul_xor_u64(b: &mut Bencher) {
+    bench_mul_xor::<u64>(b);
+}
 
-    #[bench]
-    fn bench_rem(b: &mut Bencher) {
-        let mut rng = rand::thread_rng();
-        let mut a = [0i8; 128];
-        for i in a.iter_mut() {
-            *i = rng.gen_range(-30, 30);
-        }
-        b.iter(|| {
-            a.iter().fold(0, |i, &j| (i + j) % 30)
-        })
+#[bench]
+fn bench_mul_xor_u128(b: &mut Bencher) {
+    bench_mul_xor::<u128>(b);
+}
+
+#[bench]
+fn bench_rem(b: &mut Bencher) {
+    let mut rng = rand::thread_rng();
+    let mut nums = [0i8; 128];
+    for i in nums.iter_mut() {
+        *i = rng.gen_range(0, 30);
     }
+    b.iter(|| {
+        nums.iter().fold(0, |i, &j| (i + j) % 30)
+    })
+}
+
+#[bench]
+fn bench_rem_branch(b: &mut Bencher) {
+    let mut rng = rand::thread_rng();
+    let mut nums = [0u8; 128];
+    for i in nums.iter_mut() {
+        *i = rng.gen_range(0, 30);
+    }
+    b.iter(|| {
+        nums.iter().fold(0, |i, &j| {
+            let sum = i + j;
+            if sum < 30 {
+                sum
+            } else {
+                sum - 30
+            }
+        })
+    })
 }
